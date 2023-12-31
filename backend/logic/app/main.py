fe7384c8666acae404e3b1f422cd1f5df33a2cc9
kolaxy <vybornoff@outlook.com>
2023-12-31 11:08:22 +0300

    added RMQ

--- backend/logic/app/main.py
+++ backend/logic/app/main.py
@@ -1,22 +1,16 @@
-from fastapi import Depends, FastAPI, HTTPException, status
-from database import engine
-from tickets import models as tickets_models
-from tickets import main as tickets_main
-from messages import models as messages_models
-from messages import main as messages_main
-from users import main as users_main
-from users import models as users_models 
+from fastapi import FastAPI
+import pika
 
-tickets_models.Base.metadata.create_all(bind=engine)
-messages_models.Base.metadata.create_all(bind=engine)
-users_models.Base.metadata.create_all(bind=engine)
+app = FastAPI()
 
-app = FastAPI(title="Tickets system", description="A tickets system made with FastAPI")
 
-app.include_router(tickets_main.router)
-app.include_router(messages_main.router)
-app.include_router(users_main.router)
-
-@app.get("/")
-async def root():
-    return {"message": "backend server is running", "connected_modules": ["tickets", "messages"]}
+@app.get("/send_message")
+def send_message(message_text: str): 
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters('myrabbitmq'))
+    channel = connection.channel()
+    channel.queue_declare(queue='my_queue')
+    channel.basic_publish(exchange='', routing_key='my_queue',
+                          body=message_text)
+    connection.close()
+    return {"message": f'Message sent: "{message_text}"'}
--- backend/logic/app/users/settings.py
+++
@@ -1,5 +0,0 @@
-import os
-
-SECRET_KEY = os.getenv("SECRET_KEY", default="secret_key")
-ALGORITHM = os.getenv("ALGORITHM", default="HS256")
-ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", default=30)
